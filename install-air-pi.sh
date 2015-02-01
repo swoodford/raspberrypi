a64bf636f313059a13d7cfbd7909f6daf9c4e88b
Shawn Woodford <shawn@kingandpartners.com>
2015-02-01 01:52:00 -0500

    updated

--- install-air-pi.sh
+++ install-air-pi.sh
@@ -2,7 +2,7 @@
 # This script will install the Shairport Airplay emulator for Raspberry Pi
 
 # Install deps
-sudo apt-get install -y git libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils libmodule-build-perl
+sudo apt-get install -y git libao-dev libssl-dev libcrypt-openssl-rsa-perl libio-socket-inet6-perl libwww-perl avahi-utils libmodule-build-perl libavahi-client-dev libasound2-dev libpulse-dev
 
 # Install perl net sdp
 git clone https://github.com/njh/perl-net-sdp.git perl-net-sdp
@@ -14,11 +14,23 @@ sudo ./Build install
 cd ~
 
 # Install shairport
-git clone https://github.com/hendrikw82/shairport.git
+git clone git@github.com:abrasive/shairport.git
 cd shairport
+./configure
 make
 
+# Optionally install it to run at boot
+sudo make install
+cp shairport.init.sample /etc/init.d/shairport
+cd /etc/init.d
+chmod a+x shairport
+update-rc.d shairport defaults
+echo "change DAEMON_ARGS to -w \$PIDFILE -a AirPi"
+read -p "Press any key to continue..."
+sudo nano shairport
+
+
 # Start up Airplay
-./shairport.pl -a AirPi &
+./shairport -a AirPi &
 
 echo "Airplay started as device: AirPi"
