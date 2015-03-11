cc9b0fc8095da78ac228dbc9da05e7caf83e5ca0
Shawn Woodford <sw@shawnwoodford.com>
2015-03-11 17:36:12 -0400

    add date to avoid duplicate status issue

--- tweet-cputemp.py
+++ tweet-cputemp.py
@@ -4,7 +4,7 @@
 # This script will tweet your current Raspberry Pi CPU temperature
 # Requires Twython, API credentials set as env vars
 
-import sys, os
+import sys, os, time
 from twython import Twython
 
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
@@ -16,4 +16,7 @@ api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
 
 cmd = 'awk \'{printf("%.1f °F\\n",$1*1.8/1e3)}\' /sys/class/thermal/thermal_zone0/temp'
 temp = os.popen(cmd).readline()
-api.update_status(status='My current #RaspberryPi CPU temperature is '+temp+'')
+cmd2 = 'date'
+time = os.popen(cmd2).readline()
+# api.update_status(status='My current #RaspberryPi CPU temperature is '+temp+' on '+time.strftime("%c"))
+api.update_status(status='My current #RaspberryPi CPU temperature is '+temp+' on '+time+'')
