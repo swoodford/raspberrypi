f7a38114dc56e795b6b6ffa40d31b9a8e1d02c85
Shawn Woodford <sw@shawnwoodford.com>
2015-03-19 11:14:31 -0400

    cleanup

--- tweet-status.py
+++ tweet-status.py
@@ -2,10 +2,13 @@
 
 # This script will tweet the text that is passed as an argument
 # Requires Twython, API credentials set as env vars
+# Usage: python tweet-status.py 'Hello Everyone, this is my Raspberry Pi tweeting you more nonsense'
 
-import sys, os
+import sys
+import os
 from twython import Twython
 
+# Set Twitter Credentials from environment variables
 CONSUMER_KEY = os.getenv("CONSUMER_KEY")
 CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 ACCESS_KEY = os.getenv("ACCESS_KEY")
@@ -13,4 +16,5 @@ ACCESS_SECRET = os.getenv("ACCESS_SECRET")
 
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 
-api.update_status(status=sys.argv[1])
+# Tweet
+api.update_status(status=sys.argv[1][:140])
