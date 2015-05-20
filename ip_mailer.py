05b249c8894763ae1224b274df0d58aef13aea03
Shawn Woodford <sw@shawnwoodford.com>
2015-05-20 10:50:48 -0400

    syntax

--- ip_mailer.py
+++ ip_mailer.py
@@ -3,7 +3,7 @@
 
 import subprocess
 import smtplib
-import socket
+# import socket
 from email.mime.text import MIMEText
 import datetime
 
@@ -19,12 +19,12 @@ smtpserver.login(gmail_user, gmail_password)
 today = datetime.date.today()
 
 # Very Linux Specific
-arg='ip route list'
-p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+arg = 'ip route list'
+p = subprocess.Popen(arg, shell=True, stdout=subprocess.PIPE)
 data = p.communicate()
 split_data = data[0].split()
-ipaddr = split_data[split_data.index('src')+1]
-my_ip = 'Your IP is %s' %  ipaddr
+ipaddr = split_data[split_data.index('src') + 1]
+my_ip = 'Your IP is %s' % ipaddr
 msg = MIMEText(my_ip)
 msg['Subject'] = 'IP For RaspberryPi on %s' % today.strftime('%b %d %Y')
 msg['From'] = gmail_user
