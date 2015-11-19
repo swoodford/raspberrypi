c66b30dcf9e6511831d084099726b4d8e1e03926
Shawn Woodford <sw@shawnwoodford.com>
2015-11-19 09:46:53 -0500

    moved twitter related scripts to twitter repo

--- twitter-api-creds.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# This script sets Twitter API credentials
-
-import os
-
-os.environ["CONSUMER_KEY"] = "YOUR KEY HERE"
-os.environ["CONSUMER_SECRET"] = "YOUR KEY HERE"
-os.environ["ACCESS_KEY"] = "YOUR KEY HERE"
-os.environ["ACCESS_SECRET"] = "YOUR KEY HERE"
