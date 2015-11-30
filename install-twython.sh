d01008fc15ac2c0f5df50ccd788073f9281bf3d2
Shawn <swoodford@users.noreply.github.com>
2015-11-30 14:47:41 -0500

    Need python-dev also

--- install-twython.sh
+++ install-twython.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python-setuptools libffi-dev libssl-dev -y
+sudo apt-get install python-dev python-setuptools libffi-dev libssl-dev -y
 sudo easy_install pip
 sudo pip install pyopenssl ndg-httpsclient pyasn1
 sudo pip install twython
