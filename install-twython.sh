cceb933060643f60f66b6ab90cd33c20311c3943
Shawn <swoodford@users.noreply.github.com>
2015-11-30 14:38:19 -0500

    Add some other possible prerequisites

--- install-twython.sh
+++ install-twython.sh
@@ -4,8 +4,9 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install python-setuptools -y
+sudo apt-get install python-setuptools libffi-dev libssl-dev -y
 sudo easy_install pip
+sudo pip install pyopenssl ndg-httpsclient pyasn1
 sudo pip install twython
 
 echo "Twython Installed."
