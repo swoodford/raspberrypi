e9a55015b5e74fb6629bb0315bdba5a7911e62bd
Shawn Woodford <sw@shawnwoodford.com>
2016-02-16 01:05:07 -0500

    final tweaks

--- speedtest.py
+++ speedtest.py
@@ -11,7 +11,7 @@
 import csv
 import datetime
 import os
-import sys
+# import sys
 import time
 
 import twitter
@@ -19,27 +19,27 @@ import twitter
 
 def test():
 
-        # run speedtest-cli
+        # Run speedtest-cli
         print 'Running speedtest...'
         a = os.popen("python /usr/local/bin/speedtest-cli --simple").read()
-        print 'ran'
-        # split the 3 line result (ping,down,up)
+        print 'Speedtest completed:'
+        # Split the 3 line result (ping,down,up)
         lines = a.split('\n')
         print a
         ts = time.time()
         date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        # if speedtest could not connect set the speeds to 0
+        # If speedtest could not connect set the speeds to 0
         if "Cannot" in a:
                 p = 100
                 d = 0
                 u = 0
-        # extract the values for ping down and up values
+        # Extract the values for ping down and up values
         else:
                 p = lines[0][6:11]
                 d = lines[1][10:14]
                 u = lines[2][8:12]
         print date, p, d, u
-        # save the data to file for local network plotting
+        # Save the data to file for local network plotting
         out_file = open('/home/pi/speedtest.csv', 'a')
         writer = csv.writer(out_file)
         writer.writerow((ts * 1000, p, d, u))
@@ -58,7 +58,7 @@ def test():
         if "Cannot" in a:
                 print "Internet seems to be down."
                 try:
-                        tweet = "Hey @TWC @TWC_Help why is my internet down? I pay for 100down\\15up in #Westchester #NY? #twcoutage #TWC"
+                        tweet = "Hey @TWC @TWC_Help why is my #internet #down? I pay for 100 down\\10 up in #Westchester #NY? #twcoutage #TWC"
                         twit.statuses.update(status=tweet)
                 except:
                         pass
@@ -68,7 +68,7 @@ def test():
                 print "Tweeting slow bandwidth."
                 try:
                         # I know there must be a better way than to do (str(int(eval())))
-                        tweet = "Hey @TWC why is my internet speed " + str(int(eval(d))) + "down\\" + str(int(eval(u))) + "up when I pay for 100down\\15up in #Westchester #NY? @TWC_Help @TWC_NYNJ #TWC #slow #speedtest"
+                        tweet = "Hey @TWC why is my #internet #speed " + str(int(eval(d))) + "down\\" + str(int(eval(u))) + "up when I pay for 100 down\\10 up in #Westchester #NY? @TWC_Help @TWC_NYNJ #TWC #slow #speedtest"
                         twit.statuses.update(status=tweet)
                 except Exception, e:
                         print str(e)
@@ -77,4 +77,4 @@ def test():
 
 if __name__ == '__main__':
         test()
-        print 'completed'
+        print 'Completed!'
